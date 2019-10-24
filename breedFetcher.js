80e4583de3ab360d3d7e7697c58146e1efccd9d3
Sam <samuelrush@gmail.com>
2019-10-24 20:12:02 +0000

    edits to the breedfetcher before my technical interview

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,12 +1,16 @@
 const request = require("request");
-const argv = process.argv.slice(2);
-const catSite = ("https://api.thecatapi.com/v1/breeds/search?q=" + argv[0]);
+//const argv = process.argv.slice(2);
+const catSite = ("https://api.thecatapi.com/v1/breeds/search?q=");
 
-request(catSite, function(error, response, body) {
-  if (error === null) {
-    console.log("Cat not found");
-  } else {
-    const data = JSON.parse(body);
-    console.log(data[0].description);
-  }
-});
+const fetchBreedDescription = (breedName, callback) => {
+  request((catSite + breedName), function(error, response, body) {
+    if (error !== null) {
+      throw err
+    } else {
+      const data = JSON.parse(body);
+      console.log(data[0].description);
+    }
+  });
+};
+
+module.exports = { fetchBreedDescription };
